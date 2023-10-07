910fab615ed0751a4cbe435bff62980c4088ea27
Ilgiz <hairullin.in@gmail.com>
2023-10-07 20:40:10 +0300

    1.2 pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,7 @@
+import csv
+from csv import DictReader
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +11,19 @@ def index(request):
 
 
 def bus_stations(request):
+
+    with open('data-398-2018-08-30.csv', newline='', encoding='utf8') as f:
+     bus_stations = []
+     for row in DictReader(f):
+            bus_stations.append(row)
+     page_number = int(request.GET.get("page", 1))
+     paginator = Paginator(bus_stations, 10)
+     page = paginator.get_page(page_number)
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+     context = {
+         'bus_stations': page,
+         'page': page,
     }
     return render(request, 'stations/index.html', context)
