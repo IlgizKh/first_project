ed5cc99003912c408a7112886f61e0812cb53754
Ilgiz <hairullin.in@gmail.com>
2023-10-18 02:35:24 +0300

    2.1 phones

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+        name = models.CharField(max_length=50)
+        price = models.DecimalField(max_digits=12, decimal_places=2)
+        image = models.ImageField(max_length=200)
+        release_date = models.DateField()
+        lte_exists = models.BooleanField()
+        slug = models.SlugField(max_length=60, unique=True, null=False)
+
+        def save(self, *args, **kwargs):
+                if not self.slug:
+                        self.slug = slugify(self.name)
+                return super().save(*args, **kwargs)
+
