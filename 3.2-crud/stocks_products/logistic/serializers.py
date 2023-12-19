43eea6c69aa3e87977e57e326d8424acf8cbd917
Ilgiz <hairullin.in@gmail.com>
2023-12-20 02:50:04 +0300

    3.2

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,21 +1,31 @@
 from rest_framework import serializers
 
+from logistic.models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
+
     # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
     pass
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
     pass
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
-
     # настройте сериализатор для склада
-
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
@@ -26,7 +36,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            Stock.objects.get_or_create(stock=stock, **position)
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +50,7 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.update_or_create(defaults={'quantity': position['quantity'], 'price': position['price']},
+                                                  product=position['product'], stock=stock)
         return stock
